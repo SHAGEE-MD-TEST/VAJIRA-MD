aaecb84af1a5c0e8064657612f7ccf5ec4b28756
SHAGEE-MD <SHAGEEBOT@hotmail.com>
2025-01-17 20:08:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,13 +6,13 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=eFdzCSbY#dye5BjG3uRjo81YZzxltr2CsrQY45oZLC4Txzq3zBtI' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,
-N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=aQUABSSK#THtgtuJ50YWY5desJ6OaVA3E6tYF19PyWJdavaqZ7bI' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94769225032' : process.env.OWNER_NUMBER,
+N_JID: process.env.N_JID=== undefined ? '‌94769225032@s.whatsapp.net': process.env.N_JID,    
 PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
 MAX_SIZE: 500,
-STATUS_REPLY_MESSAGE: '𝐘𝐎𝐔𝐑 𝐒𝐓𝐀𝐓𝐔𝐒 𝐒𝐄𝐄𝐍 𝐉𝐔𝐒𝐓 𝐍𝐎𝐖 𝐁𝐘 𝐕𝐀𝐉𝐈𝐑𝐀 𝐌𝐃 ✅',    
+STATUS_REPLY_MESSAGE: '..',    
 ALIVE:  process.env.ALIVE  || '> VAJIRA MD'  ,    
 DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO        
 };
